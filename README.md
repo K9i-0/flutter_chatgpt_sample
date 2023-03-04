d5740ff7d187e336138c1116f2f352bfa193208c
K9i-0 <geukfyger@gmail.com>
2023-03-04 16:48:30 +0900

    ✨first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_chatgpt_sample
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/k9i/flutter_chatgpt_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.k9i.flutter_chatgpt_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
