2a558db3b716f8fd0756695b1ef18bb0326172a3
K9i-0 <geukfyger@gmail.com>
2023-03-04 17:01:27 +0900

    ✨openai apiの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:dart_openai/openai.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // .evnから環境変数を読み込む
+  await dotenv.load(fileName: '.env');
+  OpenAI.apiKey = dotenv.get('OPEN_AI_API_KEY');
+
   runApp(const MainApp());
 }
 
